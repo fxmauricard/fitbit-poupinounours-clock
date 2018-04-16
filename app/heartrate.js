0f08074600afdd703da8bb65d5375b29738c43e4
François-Xavier MAURICARD <fxmauricard@gmail.com>
2018-04-16 14:43:08 +0200

    Added heart rate sensor to header bar.

---
+++ app/heartrate.js
@@ -0,0 +1,22 @@
+import document from "document";
+import { HeartRateSensor } from "heart-rate";
+import * as util from "../common/utils";
+
+// Get a handle on the <text> element.
+const heartRateLabel = document.getElementById("heartRateLabel");
+
+// Create a new instance of the HeartRateSensor object.
+const hrm = new HeartRateSensor();
+
+hrm.onreading = function() {
+  // Update the <text> element with the current heart rate sensor value.
+  heartRateLabel.text = util.monoDigits(hrm.heartRate);
+
+  // Stop monitoring the sensor.
+  hrm.stop();
+}
+
+export function update() {
+  // Begin monitoring the sensor.
+  hrm.start();
+}
--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import clock from "clock";
 import * as activity from "activity";
 import * as battery from "battery";
 import * as date from "date";
+import * as heartRate from "heartrate";
 import * as time from "time";
 import * as util from "../common/utils";
 
@@ -10,6 +11,7 @@ clock.granularity = "seconds";
 
 // Each second.
 clock.ontick = (evt) => {
+  heartRate.update();
   battery.update();
 
   const todayDate = evt.date;
