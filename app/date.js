1c50a7da28442194b14860e4ae7175b970e1972b
François-Xavier MAURICARD <fxmauricard@gmail.com>
2018-04-16 16:03:48 +0200

    Updated UI on Fitbit Versa in order to have year in two digits formatting + HR sensor in now right aligned in a relative way in order to have the same CSS/GUI file for Fitbit Ionic and Fitbit Versa.

--- app/date.js
+++ app/date.js
@@ -1,5 +1,9 @@
 import document from "document";
 import { locale } from "user-settings";
+import { me as device } from "device";
+
+// workaround for Fitbit Ionic running Fitbit OS 1.0.
+if (!device.screen) device.screen = { width: 348, height: 250 };
 
 // Get a handle on the <text> element.
 const dateLabel = document.getElementById("dateLabel");
@@ -16,5 +20,7 @@ const monthLabels =  {
 
 export function update(todayDate) {
   // Update the <text> element with the date.
-  dateLabel.text = (dayLabels[locale.language][todayDate.getDay()] + ' ' + todayDate.getDate() + ' ' + monthLabels[locale.language][todayDate.getMonth()] + ' ' + todayDate.getFullYear()).toUpperCase();
+  const year = (device.screen.width === 348) ? todayDate.getFullYear() : todayDate.getFullYear().toString().substr(-2);
+
+  dateLabel.text = (dayLabels[locale.language][todayDate.getDay()] + ' ' + todayDate.getDate() + ' ' + monthLabels[locale.language][todayDate.getMonth()] + ' ' + year).toUpperCase();
 }
